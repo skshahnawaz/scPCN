f7621e5f9b8a60772fc60ea25c90313791b8da8c
Sk Shahnawaz <skshahnawaz2909@gmail.com>
2023-07-03 14:56:03 +0530

    dataset download fixed

--- main.py
+++ main.py
@@ -5,22 +5,24 @@ import time
 
 # https://discourse.scverse.org/t/how-to-fix-this-error-when-install-scanpy/812/2
 
-# dataset_dict = {'url': 'http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz'}
+dataset_dict = {'url': 'http://cf.10xgenomics.com/samples/cell-exp/1.1.0/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz'}
 
-# download = Download(dataset_dict)
-preprocess = Preprocess()
+# Download the dataset
+download = Download(dataset_dict)
+download.download()
 
-# download.download()
-preprocess.configure_scanpy()
-preprocess.create_anndata()
-cell_gene_data = preprocess.preprocess_data()
+# preprocess = Preprocess()
 
-# print(cell_gene_data)
+# preprocess.configure_scanpy()
+# preprocess.create_anndata()
+# cell_gene_data = preprocess.preprocess_data()
 
-# process = Process()
-process = Process(cell_gene_data) #only if data to be loaded from saved file
-process.convert_to_dataframe()
-process.fetch_enrichr_data()
-process.process_enrichr_data()
-process.find_activated_pathways()
-process.cluster_data()
+# # print(cell_gene_data)
+
+# # process = Process()
+# process = Process(cell_gene_data) #only if data to be loaded from saved file
+# process.convert_to_dataframe()
+# process.fetch_enrichr_data()
+# process.process_enrichr_data()
+# process.find_activated_pathways()
+# process.cluster_data()
